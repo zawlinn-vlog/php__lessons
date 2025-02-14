c2c2f594e3127b89288c587b4147fa57911e7925
zawlinn.vlog <zawlinn.profile@outlook.com>
2025-02-14 21:49:30 +0700

    PHP: Course Update

---
+++ 01 Exercise/index.php
@@ -0,0 +1,178 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
+</head>
+<body>
+
+<?php
+
+
+    # OUTPUT (ECHO and PRINT)
+
+    echo "HELLO PHP", "<br/>";
+
+    print "HELLO PHP";
+
+    echo "<br/>";
+
+    # Variable 
+    $firstname = 'Zaw Linn';
+    $lastname = 'Tun';
+   
+
+    echo "My name is $firstname $lastname.";
+
+    echo "<br/>";
+
+    # PRINT Return or not
+
+    $res = print "$firstname";
+
+    echo "<br/>";
+
+    echo $res;
+
+    echo "<br/>";
+
+    # ECHO Return or not
+
+    // echo "<br/>";
+
+    // $res = echo "$firstname";
+
+    // echo "<br/>";
+
+    // echo $res;
+
+    // echo "<br/>";
+
+
+    echo "My first name is \$firstname. <br/>";
+    echo "My first name is \${firstname} <br/>";
+    echo "My first name is \{$firstname} <br/>";
+    echo "My first name is {\$firstname} <br/>";
+
+    echo "My first name is {$firstname}\n My job is Developer.";
+
+    // header("Content-type: text/plain");
+
+
+    # CONCAT
+
+    $fullname = $firstname . $lastname;
+
+    echo "my name is $fullname";
+
+    $gender = 'male';
+
+    echo "<br/>";
+    echo "$gender";
+
+
+    # COMMENT 
+
+    // - single line comment // or #
+
+    // - multi line comment /* */
+
+    $int = 4000;
+
+    echo "<br/>";
+    echo $int;
+
+    echo PHP_INT_MAX;
+    echo "<br/>";
+    echo PHP_INT_MIN;
+    echo "<br/>";
+    echo PHP_INT_SIZE;
+
+
+    // $str = 'I am zawlinn from mae sot.';
+
+    // echo "<br/>";
+    // echo "The length of the string is ", strlen($str), ".";
+
+    $bol = true;
+
+    echo "<br/>";
+    echo $bol;
+
+    $test = null; # return empty
+    $test = ''; # return empty
+    echo "<br/>";
+    echo $test; 
+
+    # argv[index] & argc 
+/*
+    echo $argv[1];
+    echo $argc;
+
+*/
+
+# PHP STRING 
+$str = "This is the world, we are living.";
+
+echo "<br/>";
+
+## STRING PROPERTIES
+
+# strlen(str)-  returns the length of a string.
+   
+echo strlen($str); # 33 letter
+
+echo "<br/>";
+
+## str_word_count(str) - return the number of words.
+
+echo str_word_count($str); # 7 words
+
+## strpos(str) - find and return the index number of finding letter.
+
+echo "<br/>";
+
+echo strpos($str, 'world'); # 12 index number. begin 0 
+
+
+## MODIFY STRING
+
+### TO UPPER CASE
+
+echo "<br/>";
+echo strtoupper($str);
+
+#### TO LOWER CASE
+
+echo "<br/>";
+echo strtolower($str);
+
+#### REMOVE WHITE SPACE
+
+// $str = '      this is the world, we are living.      ';
+
+echo "<br/>";
+
+echo trim($str);
+
+#### reverse string
+
+echo "<br/>";
+
+echo strrev($str);
+
+#### STRING to SPRIT AS AN ARRAY
+
+echo "<br/>";
+$ary =  explode(' ', $str);
+
+print_r($ary);
+
+
+?>
+    
+<script src="assets/js/app.js"></script>
+</body>
+</html>
