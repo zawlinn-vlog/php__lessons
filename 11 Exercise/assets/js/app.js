6e9843b80fdf50aee579f00d6cb9ef8980b36add
zawlinn-vlog <zawlinn.profile@outlook.com>
2025-02-24 10:54:56 +0700

    PHP: Course Update

--- 11 Exercise/assets/js/app.js
+++ 11 Exercise/assets/js/app.js
@@ -5,7 +5,8 @@ let form = document.querySelector("form");
 // CHECK EMAIL
 
 function checkEmail(email) {
-  const reg = /([a-zA-Z]+)\@([a-z]+)\.([a-z]{2,3}|[a-z]{2})/;
+  // const reg = /([a-zA-Z]+)\@([a-z]+)\.([a-z]{2,3}|[a-z]{2})/;
+  const reg = /(?=.*[a-z])(?=.*|\d)(?=.*@)/;
 
   let bol = reg.test(email);
 
@@ -14,11 +15,71 @@ function checkEmail(email) {
   return bol;
 }
 
+console.log(checkEmail("zaws linn@gmail.com"));
+
 // CHECK PASSWORD
 
 function checkPassword(password) {
-  if (password.length <= 8) {
+  if (password.length >= 8) {
+    console.log("pas length is more than 8");
+    const reg =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@.#$!%*?&])[A-Za-z\d@.#$!%*?&]{8}$/;
+    const bol = reg.test(password);
+
+    return bol;
   } else {
     return false;
   }
 }
+
+function checkUsername(username) {
+  const reg = /()/;
+
+  const bol = reg.test(username);
+
+  if (bol) {
+    return bol;
+  }
+
+  return false;
+}
+
+/*
+
+const levels = {
+    1: "Very Weak",
+    2: "Weak",
+    3: "Medium",
+    4: "Strong",
+};
+
+function checkPwd(pwd) {
+    if (pwd.length > 15) {
+        return console.log(pwd + " - Too lengthy");
+    } else if (pwd.length < 8) {
+        return console.log(pwd + " - Too short");
+    }
+
+    const checks = [
+        /[a-z]/,     // Lowercase
+        /[A-Z]/,     // Uppercase
+        /\d/,        // Digit
+        /[@.#$!%^&*.?]/ // Special character
+    ];
+    let score = checks.reduce((acc, rgx) => acc + rgx.test(pwd), 0);
+
+    console.log(pwd + " - " + levels[score]);
+}
+
+let pwds = [
+    "u4thdkslfheogica",
+    "G!2ks",
+    "GeeksforGeeks",
+    "Geeks123",
+    "GEEKS123",
+    "Geeks@123#",
+];
+
+pwds.forEach(checkPwd);
+
+*/
--- 11 Exercise/index.php
+++ 11 Exercise/index.php
@@ -8,37 +8,24 @@ include_once ("./assets/modules/header.php");
 
 
 <div class="container mt-4 p-0">
-    <div class="row w-100">
-        <div class="col-3 d-none d-md-block">
+    <div class="row w-100 g-0">
+        <div class="col-3 d-none d-md-block pe-3">
            <?php include_once "./assets/modules/sidebar.php"  ?>
         </div>
 
-        <div class="col-12 col-md-9 ps-4 ms-2 pb-5" >
-            <div class="row gy-4 mx-auto " >
-                <div class="col-6 col-lg-4">
-                    <div class="card p-3">
-                        <h3 class="card-header text-secondary">Card Title &mdash;</h3>
-                        <p class="card-body text-muted">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem voluptatem sed consectetur? Sint eos quae non eius nobis quos quidem.</p>
-                        <div class="d-flex">
-                            <button class="btn btn-primary ms-auto">Read Me</button>
-                        </div>
-                    </div>
-                </div>
+        <div class="col-12 col-md-9 px-0 pb-5 " >
+            <div class="row gy-4" >
+                <!-- Cards -->
                 <div class="col-6 col-lg-4 col-md-6">
                     <div class="card p-3">
-                        <h3 class="card-header text-secondary">Card Title &mdash;</h3>
-                        <p class="card-body text-muted">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem voluptatem sed consectetur? Sint eos quae non eius nobis quos quidem.</p>
-                        <div class="d-flex">
-                            <button class="btn btn-primary ms-auto">Read Me</button>
+                        <img src="./assets/img/logo.jpg" class="card-img-top " alt="" style="height: 14rem; object-fit: cover;">
+                        <div class="card-body mt-3">
+                            <h3 class="card-title text-secondary">Card Title &mdash;</h3>
+                            <p class="card-text text-muted mt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem voluptatem sed consectetur? Sint eos...</p>
                         </div>
-                    </div>
-                </div>
-                <div class="col-6 col-lg-4 col-md-6">
-                    <div class="card p-3">
-                        <h3 class="card-header text-secondary">Card Title &mdash;</h3>
-                        <p class="card-body text-muted">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem voluptatem sed consectetur? Sint eos quae non eius nobis quos quidem.</p>
-                        <div class="d-flex">
-                            <button class="btn btn-primary ms-auto">Read Me</button>
+                        <div class="d-flex ms-auto mt-4">
+                            <a href="" class="text-link text-decoration-none me-3" >Edit</a>
+                            <a href="" class="text-link text-decoration-none">Read More</a>
                         </div>
                     </div>
                 </div>
